43d7b730650aee767c79418ad8b8a6fefb56f6de
Rohit Kushwaha <34808268+Shivampal983817@users.noreply.github.com>
2023-02-20 17:12:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Rohitxyzott/MdiskSearchBotv4.git /MdiskSearchBotv4
+cd /MdiskSearchBotv4
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
